bce2567014d0edc6dc9eedb1f45f8808b90078d7
eragon11 <erapekkao@gmail.com>
2019-08-02 12:34:09 +0530

    maxnum

--- maxnum.py
+++ maxnum.py
@@ -1,14 +1,7 @@
 arr = [int(input()) for i in range(3)];
 
-print (arr);
-
 max_no = 0;
 
-for i in arr:
-    if(i > max_no):
-        max_no = i;
-    else:
-        i = max_no;
-
+max_no = max(arr)
 
 print (max_no);
