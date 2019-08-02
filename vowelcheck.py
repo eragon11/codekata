cbe3faf913f1c50373ba99234a5f573b2640273c
eragon <sundarelavarasan@gmail.com>
2019-08-02 09:49:45 +0530

    vowelconsonant

---
+++ vowelcheck.py
@@ -0,0 +1,9 @@
+vowel = input();
+
+if ((vowel >= 'a') and (vowel<= 'z')):
+    if (vowel == 'a' or vowel == 'e'or vowel == 'i' or vowel == 'o'or vowel == 'u'):
+        print ("Vowel");
+    else:
+        print ("Consonant");
+else:
+    print ("invalid");
