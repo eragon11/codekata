c9f5020bd960847251e48f0f15782360a1521362
eragon11 <erapekkao@gmail.com>
2019-08-07 13:48:57 +0530

    rangearmstrong

---
+++ Beginner/set2/rangearmstrong.py
@@ -0,0 +1,11 @@
+armstrong = [int(i) for i in input().split()];
+
+for i in range(armstrong[0],armstrong[1]):
+    buffer = i;
+    sum =0;    
+    while (buffer > 0):
+        digit = buffer % 10;
+        sum += digit ** len(str(i));
+        buffer //=10;
+    if(sum == i):
+        print(sum);
